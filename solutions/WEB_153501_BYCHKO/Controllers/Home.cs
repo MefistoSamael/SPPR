fc31ec63f7c94e4b6b7254700e42b5c28fc96cd6
vassyan <vasa060504@mail.ru>
2023-08-24 09:43:04 +0300

    First lab done

---
+++ solutions/WEB_153501_BYCHKO/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153501_BYCHKO.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
